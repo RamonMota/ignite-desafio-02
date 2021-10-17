b393127b197fc20db30ceef7410495a05f52715b
Ramon Mota <ramon_mota@live.com>
2021-10-17 13:43:27 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import Routes from './routes';
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import { CartProvider } from './hooks/useCart';
+
+const App = (): JSX.Element => {
+  return (
+    <BrowserRouter>
+      <CartProvider>
+        <GlobalStyles />
+        <Header />
+        <Routes />
+        <ToastContainer autoClose={3000} />
+      </CartProvider>
+    </BrowserRouter>
+  );
+};
+
+//https://www.notion.so/Desafio-01-Criando-um-hook-de-carrinho-de-compras-5769216778794019a83f544e79167b12
+
+export default App;
